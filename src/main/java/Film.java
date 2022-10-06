40395bd730d4ce472203a44234401ca91efcf8e3
UxiaCeaVila <105040856+UxiaCeaVila@users.noreply.github.com>
2022-10-06 14:01:23 +0200

    v1

---
+++ src/main/java/Film.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Film {
+    private String id;
+    private String title;
+    private String original_title;
+    private String original_title_romanised;
+    private String description;
+    private String director;
+    private String producer;
+    private ArrayList<Film> films = new ArrayList<>();
+    private String species;
+}
