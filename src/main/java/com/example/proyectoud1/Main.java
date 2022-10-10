b406e38d783d8456beca02a5373a4ef67b267a60
UxiaCeaVila <105040856+UxiaCeaVila@users.noreply.github.com>
2022-10-10 14:34:57 +0200

    v4 (Funcional 1)

--- src/main/java/com/example/proyectoud1/Main.java
+++ src/main/java/com/example/proyectoud1/Main.java
@@ -2,6 +2,7 @@ package com.example.proyectoud1;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -9,11 +10,10 @@ import java.io.IOException;
 
 public class Main extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("mainview.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 343, 389);
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("mainview.fxml"));
         stage.setTitle("Studio Ghibli");
-        stage.setScene(scene);
+        stage.setScene(new Scene(root, 343, 389));
         stage.show();
 
     }
--- src/main/java/com/example/proyectoud1/controller/Controller.java
+++ src/main/java/com/example/proyectoud1/controller/Controller.java
@@ -1,7 +1,7 @@
 package com.example.proyectoud1.controller;
 
 import com.example.proyectoud1.model.Film;
-import com.example.proyectoud1.model.Response;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -11,13 +11,11 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
 import java.net.URL;
 import java.util.List;
 
-public class Controller {
+public class Controller{
     private String finalURL = "https://ghibliapi.herokuapp.com/films?title=";
 
     @FXML
@@ -39,15 +37,15 @@ public class Controller {
             URL jsonURL = new URL(finalURL + title.getText());
 
             ObjectMapper objectMapper = new ObjectMapper();
-            Response response = objectMapper.readValue(jsonURL, Response.class);
-            response.getResponse().stream().forEach(System.out::println);
-            Film film = response.getResponse().get(0);
+
+            //Response response = objectMapper.readValue(jsonURL, Response.class);
+            List<Film> myObjects = objectMapper.readValue(jsonURL, new TypeReference<List<Film>>(){});
+            Film film =myObjects.get(0);
             oTitle.setText(String.valueOf(film.getOriginalTitle()));
             rTitle.setText(String.valueOf(film.getOriginalTitleRomanised()));
             year.setText(String.valueOf(film.getReleaseDate()));
             Image banner = new Image(film.getMovieBanner(), true);
-            image.setImage(banner);
-        } catch (IOException e) {
+            image.setImage(banner);        } catch (IOException e) {
             e.printStackTrace();
         }
 
