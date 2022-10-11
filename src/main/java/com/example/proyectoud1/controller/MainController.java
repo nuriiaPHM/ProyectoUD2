f2a11d50516d6bc8c23c947ec3a99341fafff764
UxiaCeaVila <105040856+UxiaCeaVila@users.noreply.github.com>
2022-10-11 09:03:20 +0200

    v4.6(ERROR)

--- src/main/java/com/example/proyectoud1/controller/MainController.java
+++ src/main/java/com/example/proyectoud1/controller/MainController.java
@@ -1,11 +1,13 @@
 package com.example.proyectoud1.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import org.controlsfx.control.tableview2.filter.filtereditor.SouthFilter;
 
 import java.io.IOException;
 
@@ -13,11 +15,15 @@ public class MainController {
     public Button btnCharactersScreen;
     public Button btnFilmsScreen;
     public Button btnLocationScreen;
-/*
-    public void sFilm(ActionEvent actionEvent) throws IOException {
+    @FXML
+    public void sFilm(ActionEvent actionEvent) {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("film.fxml"));
 
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("com.example.proyectoud1.films.fxml"));
+
+            Parent root = loader.load();
+
+            FilmController filmController = loader.getController();
 
             Scene scene = new Scene(root);
             Stage stage = new Stage();
@@ -25,19 +31,16 @@ public class MainController {
             stage.setScene(scene);
             stage.show();
 
-            stage.setOnCloseRequest(e -> FilmController.closeWindows);
+            stage.setOnCloseRequest(e -> filmController.closeWindows());
             Stage myStage = (Stage) this.btnFilmsScreen.getScene().getWindow();
             myStage.close();
 
 
-            stage.setTitle("Studio Ghibli");
-            stage.setScene(new Scene(root, 379, 164));
-            stage.show();
         } catch (IOException e) {
-
+            System.out.println(e.getMessage());
         }
     }
-
+/*
     public void sPeople(ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("film.fxml"));
         stage.setTitle("Studio Ghibli");
@@ -50,6 +53,6 @@ public class MainController {
         stage.setTitle("Studio Ghibli");
         stage.setScene(new Scene(root, 379, 164));
         stage.show();
-    }
-*/
+    }*/
+
 }
